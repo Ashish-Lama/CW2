9769e18f2d73a1aa5d579f92b15330d28d1cfc4d
onenonlyfreak <ashishlama4444@gmail.com>
2022-04-21 00:52:37 +0100

    you can view option for normal mode

--- button.js
+++ button.js
@@ -3,6 +3,18 @@ var button= document.getElementById("Normal");
 
 button.onclick= function(){
 
-if
+if(Live.className=="open"){
+    Live.className="";
+    Easy.className="";
+    Hard.className="";
+    button.innerHTML="Normal Mode ";
+} else {
+    Live.className="open";
+    Easy.className="open";
+    Hard.className="open";
+    button.innerHTML="Show Live mode";
 
+  
+    
 }
+};
